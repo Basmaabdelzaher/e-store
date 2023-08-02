e364e6553e4dadb8db590a0769b51671cbdc271c
basma ismail <plfl-basism@moca.main>
2023-08-02 13:13:41 +0300

    shoppingcart update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,23 @@
 import React, { useContext } from "react";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoArrowForwardSharp } from "react-icons/io5";
+
 import { ShoppingCartContext } from "../context/ShoppingCartContext";
 const Sidebar = () => {
-    const {showSidebar, setShowSidebar} = useContext(ShoppingCartContext);
+  const { showSidebar, setShowSidebar } = useContext(ShoppingCartContext);
 
-    return (
-    <div className={`sidebar ${showSidebar? 'show-sidebar' : ''}`}>
-        <IoClose onClick={() => setShowSidebar(false)}/>
-        <p>I am the sidebar!</p>
+  return (
+    <div className={`shopping-cart card sidebar ${showSidebar ? "show-sidebar" : ""}`}>
+      <div className="card-header py-3">
+        <span className="float-left font-weight-bold">SHOPPING CART(0)</span>
+        <span className="float-right">
+          <IoArrowForwardSharp size={25} onClick={() => setShowSidebar(false)} />
+        </span>
+        
+      </div>
+      <div className="card-body">Body</div>
+      <div className="card-footer py-3">footer</div>
     </div>
-    );
+  );
 };
 
 export default Sidebar;
